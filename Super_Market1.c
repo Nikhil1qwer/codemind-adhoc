f69e1478388525d27422e7b33594c97739af3adf
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-02-13 11:17:41 +0530

    #include <stdio.h> #include <stdlib.h>
    void GetCustomerID(int *id, int size) {
        for(int i = 0; i < size; i++) {
            scanf("%d", &id[i]);
        }
    }
    
    void CountVisits(int *id, int *visits, int size) {
        for(int i = 0; i < size; i++) {
            visits[id[i]]++;
        }
    }
    
    void FrequentCustomers(int *visits) {
        int limit;
        scanf("%d", &limit);
        
        for(int frequency = 9; frequency >= 0; frequency--) {
            for (int id = 9; id >= 0; id--) {
                if(visits[id] == frequency && limit > 0) {
                    printf("%d ", id);
                    limit--;
                }
            }
        }
        
    }
    
    int main() {
        int size;
        scanf("%d", &size);
        
        int *id = (int *) malloc (sizeof(int) * size);
        int *visits = (int *) calloc (10, sizeof(int));
        
        GetCustomerID(id, size);
        CountVisits(id, visits, size);
        FrequentCustomers(visits);
        
        free(id);
        free(visits);
        return 0;
    }

--- Super_Market1.c
+++ Super_Market1.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void DisplayRegularCustomers(int *arr, int limit) {
-    int eligible = 0;
-    for (int i = 9; i > 0; i--) {
-        for (int j = 9; j >= 0; j--) {
-            if (arr[j] == i && eligible < limit) {
-                printf("%d ", j);
-                eligible++;
-            }
-        }
+void GetCustomerID(int *id, int size) {
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &id[i]);
     }
 }
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    
-    int *arr = (int *) malloc(n * sizeof(int));
-    for(int i = 0; i < n; i++){
-        scanf("%d", &arr[i]);
+void CountVisits(int *id, int *visits, int size) {
+    for(int i = 0; i < size; i++) {
+        visits[id[i]]++;
     }
-    
+}
+
+void FrequentCustomers(int *visits) {
     int limit;
     scanf("%d", &limit);
     
-    int *Vic = (int *) calloc(10, sizeof(int));
-    for (int i = 0; i < n; i++) {
-        Vic[arr[i]]++;
+    for(int frequency = 9; frequency >= 0; frequency--) {
+        for (int id = 9; id >= 0; id--) {
+            if(visits[id] == frequency && limit > 0) {
+                printf("%d ", id);
+                limit--;
+            }
+        }
     }
     
-    DisplayRegularCustomers(Vic, limit);
+}
+
+int main() {
+    int size;
+    scanf("%d", &size);
     
-    free(arr);
-    free(Vic);
+    int *id = (int *) malloc (sizeof(int) * size);
+    int *visits = (int *) calloc (10, sizeof(int));
+    
+    GetCustomerID(id, size);
+    CountVisits(id, visits, size);
+    FrequentCustomers(visits);
+    
+    free(id);
+    free(visits);
+    return 0;
 }
